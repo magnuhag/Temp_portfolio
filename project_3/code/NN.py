8611159bd5eaf50e42803ba385671c555d8aca2c
Magnus Hagen <magnuhag@uio.no>
2021-01-29 16:04:57 +0100

    Add files via upload

---
+++ project_3/code/NN.py
@@ -0,0 +1,235 @@
+class NeuralNet:
+
+    def __init__(self, X_data, Y_data, layer_sizes, num_iters, eta, act, g, lam, mini):
+        import numpy as np
+        from sklearn.model_selection import train_test_split
+
+        self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(X_data, Y_data, test_size = 0.2)
+
+        self.layer_sizes = layer_sizes
+        self.num_iters = num_iters
+        self.eta = eta
+        if act == "Sigmoid":
+            self.act = lambda x: 1/(1+np.exp(-x))
+            self.d_act = lambda x: np.exp(-x)/(1+np.exp(-x))**2
+        elif act == "RELU":
+            self.act = lambda x: np.maximum(x, 0)
+            def d_act(x):
+                x[x<=0] = 0
+                x[x>0] = 1
+                return x
+            self.d_act = d_act
+        elif act == "Leaky_RELU":
+            self.act = lambda x: np.maximum(x, 0.01 * x)
+            def d_act(x):
+                alpha = 0.01
+                dx = np.ones_like(x)
+                dx[x < 0] = alpha
+                return dx
+            self.d_act = d_act
+        self.g = g
+        self.lam = lam
+        self.mini = mini
+
+    def initialize_params(self):
+        params = {}
+        #initializing the weights and biases with random values
+        #Could be nice to be able to specify intial weights and biases as function arguments,
+        #but that might be superfluous
+        for i in range(1, len(self.layer_sizes)):
+            params["weight" + str(i)] = np.random.randn(self.layer_sizes[i], self.layer_sizes[i-1])*0.01
+            params["bias" + str(i)] = np.random.randn(self.layer_sizes[i],1)*0.01
+        return params
+
+    def feed_forward(self, params, X):
+
+        layers = len(params)//2
+        values = {}
+
+        for i in range(1, layers+1):
+            #feeding data to the input layer
+            if i==1:
+                #input to node
+                values["z" + str(i)] = np.dot(params["weight" + str(i)], X.T) + params["bias" + str(i)]
+                #activating node
+                values["a" + str(i)] = self.act(values["z" + str(i)])
+
+            #feeding forward to next layer
+            else:
+                values["z" + str(i)] = np.dot(params["weight" + str(i)], values["a" + str(i-1)]) + params["bias" + str(i)]
+                #In the regression case: output values are equal to the input in the output layer
+                if i==layers and self.g == "reg":
+                    values["a"+ str(i)] = values["z" + str(i)]
+                #In the classification case: output values are probabilities of correct prediction
+                elif i==layers and self.g == "clas":
+                    a = self.act(values["z" + str(i)])
+                    exp_term = np.exp(a)
+                    probabilities = exp_term / np.sum(exp_term, axis=1, keepdims=True)
+                    values["a" + str(i)] = probabilities
+                #The activation is equal in both regression and classification, but
+                #one should test the different ones for the best case use
+                else:
+                    values["a" + str(i)] = self.act(values["z" + str(i)])
+        return values
+
+    def backprop(self, params, values):
+        layers = len(params)//2
+        m = len(self.Y_train)
+        grads = {}
+
+        #In the regression case:
+        if self.g == "reg":
+            #Starting at the output layer, going back
+            for i in range(layers,0,-1):
+                #Finding MSE of output layer
+                #Might add different cost function options
+                if i==layers:
+                    dA = 1/m * np.sum((values["a" + str(i)] - self.Y_train)**2, axis=0, keepdims = True)
+                    dZ = dA
+                #Propage error backward
+                else:
+                    dA = np.dot(params["weight" + str(i+1)].T, dZ)
+                    dZ = np.multiply(dA, d_act(values["a" + str(i)]))
+                #If at input layer
+                if i==1:
+                    grads["weight" + str(i)] = 1/m*np.dot(dZ, self.X_train.T)
+                    #Regularization term. If lam = 0: no regularization
+                    grads["weight" + str(i)] += self.lam * params["weight" + str(i)]
+                    grads["bias" + str(i)] = 1/m*np.sum(dZ, axis=1, keepdims=True)
+                #Else at hidden hidden layer
+                else:
+                    grads["weight" + str(i)] = 1/m*np.dot(dZ,values["a" + str(i-1)].T)
+                    #Regularization term
+                    grads["weight" + str(i)] += self.lam * params["weight" + str(i)]
+                    grads["bias" + str(i)] = 1/m*np.sum(dZ, axis=1, keepdims=True)
+
+            return grads
+
+        #In the classification case:
+        elif self.g == "clas":
+            for i in range(layers,0,-1):
+                #First: calculate output error.
+                if i==layers:
+                    dA = values["a" + str(i)] - self.Y_train.T
+                    dZ = dA
+                #Propagate error backwards through layers
+                else:
+                    dA = np.dot(params["weight" + str(i+1)].T, dZ)
+                    dZ = dA @ self.act(values["a" + str(i)]).T@(1-self.act(values["a" + str(i)]))
+                #Input layer
+                if i==1:
+                    grads["weight" + str(i)] = np.dot(dZ, self.X_train)
+                    #L2 regularization (ridge regression)
+                    grads["weight" + str(i)] += self.lam * params["weight" + str(i)]
+                    grads["bias" + str(i)] = np.sum(dZ, axis=1, keepdims=True)
+                #Hidden layer(s) if any
+                else:
+                    grads["weight" + str(i)] = np.dot(dZ,values["a" + str(i-1)].T)
+                    #L2 regularization
+                    grads["weight" + str(i)] += self.lam * params["weight" + str(i)]
+                    grads["bias" + str(i)] = np.sum(dZ, axis=1, keepdims=True)
+            return grads
+
+    def update_params(self, params, grads):
+        layers = len(params)//2
+        params_updated = {}
+        #Updating the weights and biases by the gradient descent method
+        for i in range(1,layers+1):
+            params_updated["weight" + str(i)] = params["weight" + str(i)] - self.eta * grads["weight" + str(i)]
+            params_updated["bias" + str(i)] = params["bias" + str(i)] - self.eta * grads["bias" + str(i)]
+        return params_updated
+
+    def model(self):
+        params = NeuralNet.initialize_params(self)
+        #if using mini-batches:
+        min = mini[0]
+
+        if min == True:
+            data_indices = len(self.X_train)
+            #Batch size as specified in the mini list argument
+            batch_size = int(self.mini[1])
+            #Number of epochs as specified in the mini list argument
+            epochs = int(self.mini[2])
+
+            for k in range(epochs):
+                acs = np.zeros(self.num_iters)
+                for i in range(self.num_iters):
+                    #Getting the accuracy/ error to check for overfitting
+                    train_acc, test_acc = NeuralNet.compute_accuracy(self, params)
+                    acs[i] = test_acc
+                    #Creating mini-batch incdices for the design/ feature matrix and targets
+                    #Might be a problem that we risk drawing same samples multiple times during one epoch. Don't know
+                    chosen_datapoints = np.random.choice(data_indices, size=batch_size, replace=False)
+                    #Creating a mini-batch
+                    X = self.X_train[chosen_datapoints]
+                    Y = self.Y_train[chosen_datapoints]
+
+                    #Train on mini batch
+                    values = NeuralNet.feed_forward(self, params, self.X_train)
+                    #Propagate error
+                    grads = NeuralNet.backprop(self, params, values)
+                    #Update weights and biases
+                    params = NeuralNet.update_params(self, params, grads)
+                    #In case of overfitting. Sloppy fix. Needs replacements.
+                    if i>=1 and acs[i]>acs[i-1]:
+                        break
+                    #Might use if i>=1 and abs(acs[i]-acs[i-1]>some_value:
+                        #break
+                    #Would need testing. Reluctant to introduce another adjustable parameter.
+
+        elif min == False:
+            #Training the netowrk
+            for i in range(self.num_iters):
+                values = NeuralNet.feed_forward(self, params)
+                grads = NeuralNet.backprop(self, params, values)
+                params = NeuralNet.update_params(self, params, grads)
+
+        return params
+
+    def compute_accuracy(self, params):
+
+
+        values_train = NeuralNet.feed_forward(self, params, self.X_train)
+        values_test = NeuralNet.feed_forward(self, params, self.X_test)
+
+        #In the case of regression we use MSE as a measure of error
+        #Might update to allow different cost functions
+
+        if self.g == "reg":
+            train_acc = np.mean((self.Y_train + values_train["a" + str(len(layer_sizes)-1)].T)**2)
+            test_acc = np.mean((self.Y_test + values_test["a" + str(len(layer_sizes)-1)].T)**2)
+            return train_acca, test_acc
+        #In the case of classification we use percentage of correctly predicted values
+        elif self.g == "clas":
+            train_acc = 0
+            for i in range(len(self.Y_train)):
+                #Finding the true value
+                true = np.argmax(self.Y_train[i])
+                #Finding training value with highest probability
+                pred = np.argmax(values_train["a" + str(len(layer_sizes)-1)].T[i])
+
+                if true == pred:
+                    train_acc += 1
+                else:
+                    continue
+            #percentage of correct predictions (divided by 100)
+            train_acc /= len(self.Y_train)
+
+            #Test accuracy
+            #Same as for the training error/ accuracy
+            test_acc = 0
+            for i in range(len(self.Y_test)):
+                true = np.argmax(self.Y_test[i])
+                pred = np.argmax(values_test["a" + str(len(layer_sizes)-1)].T[i])
+                if true == pred:
+                    test_acc += 1
+                else:
+                    continue
+            test_acc /= len(self.Y_test)
+
+            return train_acc,  test_acc
+
+    def predict(self):
+        params = NeuralNet.model(self, X_train, Y_train)
+        prediction = NeuralNet.Feed_forward(self, X_train)
+        return prediction
---
+++ project_3/code/pr3.py
@@ -0,0 +1,483 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_diabetes
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression, LassoCV, RidgeCV
+from sklearn import tree
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import BaggingRegressor, AdaBoostRegressor, RandomForestRegressor, GradientBoostingRegressor
+from NN import NeuralNet
+import time
+
+def bias(data, model):
+    """
+    function for computing bias of data
+    """
+    return np.mean((data-np.mean(model))**2)
+def variance(data, model):
+    """
+    function for computing variance of data
+    """
+    return np.mean((model-np.mean(model))**2)
+
+
+"""
+Preparing the data:
+"""
+
+#Loading in the data into a pandas dataframe
+dataset = pd.read_csv("2019.csv")
+
+
+#Our target is the Score.
+y_a = dataset["Score"]
+#Converting y_a into array
+y = np.asarray([y_a[i] for i in range(len(y_a))])
+
+#Useless metric
+dataset.pop("Overall rank")
+#Don't want our target in the design matrix
+dataset.pop("Score")
+#Don't need this one
+dataset.pop("Country or region")
+#Creating design matrix
+X = dataset.values
+
+
+"""
+Principal component analysis
+"""
+
+scaler = StandardScaler()
+#Scaling the transpose of X for PCA
+scaler.fit(X.T)
+X_scaled = scaler.transform(X.T)
+#Doing PCA
+pca = PCA()
+pca.fit(X_scaled.T)
+pca_data = pca.transform(X_scaled.T)
+#Rounding off the percentages
+per_val = np.round(pca.explained_variance_ratio_*100, decimals = 1)
+#Labels of PC for plotting
+labels = dataset.keys()
+labels2 = np.linspace(1,len(labels), len(labels))
+
+#Making a bar plot of the contribution of each principal component
+plt.bar(x=range(1,len(per_val)+1), height = per_val, tick_label = labels2)
+plt.title("Principal Component Analysis of dataset")
+plt.ylabel("Percentage of explained values")
+plt.xlabel("Principal component")
+plt.show()
+
+X_full = dataset.values
+
+#Deleting PC6 from dataset to create reduced dataset
+dataset.pop("Perceptions of corruption")
+
+X_reduced = dataset.values
+
+#For plotting GDP per captia against happiness score
+gdp = dataset["GDP per capita"]
+
+
+plt.plot(gdp, y, "o")
+plt.xlabel("GPD per capita")
+plt.ylabel("Happiness Score")
+plt.title("Happiness as a function of wealth")
+plt.show()
+
+"""
+Model fitting: Linear regression on full dataset
+"""
+
+X = X_full
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+#Simple linear regression
+reg = LinearRegression(fit_intercept = False).fit(X_train, y_train)
+pred = reg.predict(X_test)
+
+
+print("train MSE full dataset = %g" %np.mean((y_train-X_train@reg.coef_)**2))
+print("test MSE full dataset = %g" %np.mean((y_test-pred)**2))
+
+"""
+Model fitting: Linear regression on reduced dataset
+"""
+
+X = X_reduced
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+
+reg = LinearRegression(fit_intercept = False).fit(X_train, y_train)
+pred = reg.predict(X_test)
+
+#Comparing regression with and without PC6
+print("train MSE reduced dataset = %g" %np.mean((y_train-X_train@reg.coef_)**2))
+print("test MSE reduced dataset = %g" %np.mean((y_test-pred)**2))
+
+"""
+Model fitting: RidgeCV and LassoCV, on full dataset
+"""
+
+X = X_full
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+#Regularization parameters for Ridge and Lasso regression
+alphass = np.logspace(-5, 1, 100)
+epss = np.logspace(-7, -1, 40)
+
+
+#Ridge regression with cross validation
+reg_ridge = RidgeCV(alphas = alphass, fit_intercept = False).fit(X_train, y_train)
+pred_ridge = reg_ridge.predict(X_test)
+
+#Finding mse of ridge
+print("train MSE Ridge full dataset = %g" %np.mean((y_train-X_train@reg_ridge.coef_)**2))
+print("test MSE Ridge full dataset= %g" %np.mean((y_test-pred_ridge)**2))
+
+#for plotting Lasso MSE as a function of reg.parameter
+mse_train = np.zeros(len(epss))
+mse_test = mse_train.copy()
+for i in range(len(epss)):
+    reg_lasso = LassoCV(eps = epss[i], n_alphas = 500, fit_intercept = False).fit(X_train, y_train)
+    pred_lasso = reg_lasso.predict(X_test)
+    mse_train[i] = np.mean((y_train-X_train@reg_lasso.coef_)**2)
+    mse_test[i] = np.mean((y_test-pred_lasso)**2)
+
+
+plt.plot(epss, mse_train, label = "test")
+plt.plot(epss, mse_test, label = "train")
+plt.title("MSE of LassoCV regression as function of eps, full dataset")
+plt.xlabel("eps")
+plt.ylabel("MSE")
+plt.legend()
+plt.show()
+
+
+"""
+Model fitting: RidgeCV and LassoCV, on reduced dataset
+"""
+
+X = X_reduced
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+#Regularization parameters for Ridge and Lasso regression
+alphass = np.logspace(-5, 1, 100)
+epss = np.logspace(-7, -1, 40)
+
+
+#Ridge regression with cross validation
+reg_ridge = RidgeCV(alphas = alphass, fit_intercept = False).fit(X_train, y_train)
+pred_ridge = reg_ridge.predict(X_test)
+
+#Finding mse of ridge
+print("train MSE Ridge reduced dataset = %g" %np.mean((y_train-X_train@reg_ridge.coef_)**2))
+print("test MSE Ridge reduced dataset = %g" %np.mean((y_test-pred_ridge)**2))
+
+#for plotting Lasso MSE as a function of reg.parameter
+mse_train = np.zeros(len(epss))
+mse_test = mse_train.copy()
+for i in range(len(epss)):
+    reg_lasso = LassoCV(eps = epss[i], n_alphas = 500, fit_intercept = False).fit(X_train, y_train)
+    pred_lasso = reg_lasso.predict(X_test)
+    mse_train[i] = np.mean((y_train-X_train@reg_lasso.coef_)**2)
+    mse_test[i] = np.mean((y_test-pred_lasso)**2)
+
+
+plt.plot(epss, mse_train, label = "test")
+plt.plot(epss, mse_test, label = "train")
+plt.title("MSE of LassoCV regression as function of eps, reduced dataset")
+plt.xlabel("eps")
+plt.ylabel("MSE")
+plt.legend()
+plt.show()
+
+"""
+#FFNN with full dataset
+"""
+
+# We first try to use the NN with PC6 in the dataset
+X = X_full
+# Suitable number of iterations
+num_iters = 500
+eta = 10e-5
+#activation function. Choose between "Sigmoid", "RELU", and "Leaky_RELU"
+act = "RELU"
+#g = "reg" means regression. g="clas" means classification
+g = "reg"
+#regularization parameter
+lam = 0.01
+#Whether to use mini-batches. mini = [True/ False, n_batches, n_epochs]
+#If no mini-batch: mini = [False]
+mini = [True, 60, 100]
+
+
+#Number of input nodes. Must match number of features in X
+h = int(len(X[0]))
+
+#Architecture the was found (by trial and error) to best work in this with this data
+#layer_sizes = [input_layer, hidden_layer_1, ..., hidden_layer_n, outputlayer]
+layer_sizes = [h, h, 1]
+#List of possible activation functions
+funcs = ["Sigmoid", "RELU", "Leaky_RELU"]
+#For timing the execution time of the NN
+start_time = time.time()
+for func in funcs:
+    acc_train, acc_test = NeuralNet(X, y, layer_sizes, num_iters, eta, func, g, lam, mini)
+
+    print("execution time with " + func + " function is " + str((time.time() - start_time))+ ", full dataset")
+    print("training MSE full dataset = " +str(acc_train))
+    print("test MSE full dataset = " +str(acc_test))
+
+"""
+FFNN with reduced dataset
+"""
+
+#Dataset without PC6
+X = X_reduced
+
+
+
+h = int(len(X[0]))
+
+layer_sizes = [h, h, 1]
+
+for func in funcs:
+    start_time = time.time()
+    acc_train, acc_test = NeuralNet(X, y, layer_sizes, num_iters, eta, func, g, lam, mini)
+
+    print("execution time with " + func + " function is " + str((time.time() - start_time))+ ", reduced dataset")
+    print("training MSE reduced dataset = " +str(acc_train))
+    print("test MSE reduced dataset = " +str(acc_test))
+
+
+"""
+Regression tree with reduced dataset
+"""
+
+
+X = X_reduced
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+#max possible depth of tree
+n = 14
+mse_test = np.zeros(n)
+Bias = mse_test.copy()
+Variance = Bias.copy()
+depth = np.linspace(1,n, n)
+depth = np.array([int(i) for i in range(1, len(depth)+1)])
+start_time = time.time()
+for i in depth:
+    regressor = DecisionTreeRegressor(max_depth = i).fit(X_train, y_train)
+    pred = regressor.predict(X_test)
+    Bias[i-1] = bias(y_test, pred)
+    Variance[i-1] = variance(y_test, pred)
+    mse_test[i-1] = np.mean((y_test-pred)**2)
+
+print("execution time with reduced dataset = "  + str((time.time() - start_time)))
+plt.plot(depth, mse_test, label = "MSE")
+plt.plot(depth, Bias, label = "Bias")
+plt.plot(depth, Variance, label = "Variance")
+plt.legend()
+plt.title("Regression tree: : Bias Variance as a function of depth, reduced dataset")
+plt.xlabel("Depth")
+plt.ylabel("MSE")
+plt.show()
+
+
+"""
+Regression tree with full dataset
+"""
+
+X = X_full
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+n = 14
+mse_test = np.zeros(n)
+Bias = mse_test.copy()
+Variance = Bias.copy()
+depth = np.linspace(1,n, n)
+depth = np.array([int(i) for i in range(1, len(depth)+1)])
+start_time = time.time()
+for i in depth:
+    regressor = DecisionTreeRegressor(max_depth = i).fit(X_train, y_train)
+    pred = regressor.predict(X_test)
+    Bias[i-1] = bias(y_test, pred)
+    Variance[i-1] = variance(y_test, pred)
+    mse_test[i-1] = np.mean((y_test-pred)**2)
+
+print("execution time with reduced dataset = "  + str((time.time() - start_time)))
+plt.plot(depth, mse_test, label = "MSE")
+plt.plot(depth, Bias, label = "Bias")
+plt.plot(depth, Variance, label = "Variance")
+plt.legend()
+plt.title("Regression tree: : Bias Variance as a function of depth, full dataset")
+plt.xlabel("Depth")
+plt.ylabel("MSE")
+plt.show()
+
+
+
+"""
+Random forest reduced dataset
+"""
+
+X = X_reduced
+
+n = 14
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+depth = np.linspace(1,n, n)
+depth = np.array([int(i) for i in range(1, len(depth)+1)])
+
+mse_test = np.zeros(n)
+Bias = mse_test.copy()
+Variance = Bias.copy()
+start_time = time.time()
+for i in depth:
+    reg = RandomForestRegressor(max_depth = i).fit(X_train, y_train)
+    pred = reg.predict(X_test)
+    mse_test[i-1] = np.mean((y_test-pred)**2)
+    Bias[i-1] = bias(y_test, pred)
+    Variance[i-1] = variance(y_test, pred)
+print("execution time with reduced dataset = "  + str((time.time() - start_time)))
+plt.plot(depth, mse_test, label = "MSE")
+plt.plot(depth, Bias, label = "Bias")
+plt.plot(depth, Variance, label = "Variance")
+plt.legend()
+plt.title("Random forest: Bias Variance as a function of depth, reduced dataset")
+plt.xlabel("Depth")
+plt.ylabel("MSE")
+plt.show()
+
+"""
+Random forest full dataset
+"""
+
+X = X_full
+
+n = 14
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+depth = np.linspace(1,n, n)
+depth = np.array([int(i) for i in range(1, len(depth)+1)])
+
+mse_test = np.zeros(n)
+Bias = mse_test.copy()
+Variance = Bias.copy()
+start_time = time.time()
+
+for i in depth:
+    reg = RandomForestRegressor(max_depth = i).fit(X_train, y_train)
+    pred = reg.predict(X_test)
+    mse_test[i-1] = np.mean((y_test-pred)**2)
+    Bias[i-1] = bias(y_test, pred)
+    Variance[i-1] = variance(y_test, pred)
+
+print("execution time with full dataset = "  + str((time.time() - start_time)))
+plt.plot(depth, Bias, label = "Bias")
+plt.plot(depth, Variance, label = "Variance")
+plt.plot(depth, mse_test, label = "MSE")
+plt.legend()
+plt.title("Random forest: Bias Variance as a function of depth, full dataset")
+plt.xlabel("Depth")
+plt.ylabel("Magnitude")
+plt.show()
+
+
+"""
+Bagging (Trees)
+"""
+
+X = X_full
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+n = 50
+Bias = np.zeros(n)
+Variance = np.zeros(n)
+mse_test = Bias.copy()
+
+ns = np.linspace(1,n,n)
+for i in range(50):
+    reg = BaggingRegressor(n_estimators = int(i)+1).fit(X_train, y_train)
+    pred = reg.predict(X_test)
+    mse_test[i] = np.mean((y_test-pred)**2)
+    Bias[i] = bias(y_test, pred)
+    Variance[i] = variance(y_test, pred)
+plt.plot(ns, Bias, label = "bias")
+plt.plot(ns, Variance, label = "variance")
+plt.plot(ns, mse_test, label = "MSE test")
+plt.title("Bagging: Bias Variance Trade-off with training MSE")
+plt.xlabel("Number of trees")
+plt.ylabel("Magnitude")
+plt.legend()
+plt.show()
+
+"""
+Gradient Boosting
+"""
+
+
+X = X_full
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+n = 50
+
+Bias = np.zeros(n)
+Variance = np.zeros(n)
+mse_test = Bias.copy()
+#max_depth = 3
+ns = np.linspace(1,n,n)
+for i in range(50):
+    reg = GradientBoostingRegressor(n_estimators = int(i)+1).fit(X_train, y_train)
+    pred = reg.predict(X_test)
+    mse_test[i] = np.mean((y_test-pred)**2)
+    Bias[i] = bias(y_test, pred)
+    Variance[i] = variance(y_test, pred)
+plt.plot(ns, Bias, label = "bias")
+plt.plot(ns, Variance, label = "variance")
+plt.plot(ns, mse_test, label = "MSE test")
+plt.title("Gradient Boost: Bias Variance Trade-off with training MSE")
+plt.xlabel("Number of trees")
+plt.ylabel("Magnitude")
+plt.legend()
+plt.show()
+
+"""
+AdaBoost
+"""
+
+X = X_full
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+n = 50
+
+Bias = np.zeros(n)
+Variance = np.zeros(n)
+mse_test = Bias.copy()
+#max_depth = 3
+ns = np.linspace(1,n,n)
+for i in range(50):
+    reg = AdaBoostRegressor(n_estimators = int(i)+1, learning_rate = 0.1).fit(X_train, y_train)
+    pred = reg.predict(X_test)
+    mse_test[i] = np.mean((y_test-pred)**2)
+    Bias[i] = bias(y_test, pred)
+    Variance[i] = variance(y_test, pred)
+plt.plot(ns, Bias, label = "bias")
+plt.plot(ns, Variance, label = "variance")
+plt.plot(ns, mse_test, label = "MSE test")
+plt.title("AdaBoost: Bias Variance Trade-off with training MSE")
+plt.xlabel("Number of trees")
+plt.ylabel("Magnitude")
+plt.legend()
+plt.show()
+
+#SoUnDcLoUdRaPpEr69
